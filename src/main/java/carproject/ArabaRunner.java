312f1f80bf7514ca4dcb922f2335b7295e5c8cff
busra idil <diyetisyenbusraidil@gmail.com>
2023-06-17 10:19:21 +0300

    V1

---
+++ src/main/java/carproject/ArabaRunner.java
@@ -0,0 +1,12 @@
+package carproject;
+
+public class ArabaRunner {
+    public static void main(String[] args) {
+
+        Araba araba1 = new Araba("Mercedes", "Siyah", 2000, 1800);
+        //System.out.println(araba1.getModel() +" "+araba1.getRenk()+" "+araba1.getMotor()+" "+araba1.getYil());
+        System.out.println(araba1);
+
+
+    }
+}
